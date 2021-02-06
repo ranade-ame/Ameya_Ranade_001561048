c0730bef2130a5d93a9407f122d9d8fc63fe8e3f
ranade-ame <ranade.ame@northeastern.edu>
2021-02-06 10:49:55 -0500

    Submission of AED Lab Assignment 1 by Ameya Ranade

---
+++ AED_Assignment1_Ameya Ranade/src/Data/Person.java
@@ -0,0 +1,293 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Data;
+
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author ameya
+ */
+public class Person {
+    
+    private String F_name; //Demographic Information
+    private String L_name;
+    private String Dob;
+    private String Age;    
+    private String Hgt;
+    private String Wgt;
+    private String Ssn;
+    
+    private String Str_addr; //Address Information
+    private String City;
+    private String State;
+    private String Zip;
+    
+    private String Save_name; //Saving account information
+    private String Save__rnum;
+    private String Save__accnum;
+    private String Save__accbal;
+    private String Save__acctype = "Savings";
+    
+    private String Chk_name; //Checking account information
+    private String Chk__rnum;
+    private String Chk__accnum;
+    private String Chk__accbal;
+    private String Chk__acctype = "Checking";
+    
+    private String Lcnsnum; //Driver's license information
+    private String Iss_dt;
+    private String Exp_dt;
+    private String Bld_type;
+    private String Picture;
+    
+    private String M_rcrdnum; //Medical Information
+    private String Alrg_1;
+    private String Alrg_2;
+    private String Alrg_3;
+
+    public String getF_name() {
+        return F_name;
+    }
+
+    public void setF_name(String F_name) {
+        this.F_name = F_name;
+    }
+
+    public String getL_name() {
+        return L_name;
+    }
+
+    public void setL_name(String L_name) {
+        this.L_name = L_name;
+    }
+
+    public String getDob() {
+        return Dob;
+    }
+
+    public void setDob(String Dob) {
+        this.Dob = Dob;
+    }
+
+    public String getAge() {
+        return Age;
+    }
+
+    public void setAge(String Age) {
+        this.Age = Age;
+    }
+
+    public String getHgt() {
+        return Hgt;
+    }
+
+    public void setHgt(String Hgt) {
+        this.Hgt = Hgt;
+    }
+
+    public String getWgt() {
+        return Wgt;
+    }
+
+    public void setWgt(String Wgt) {
+        this.Wgt = Wgt;
+    }
+
+    public String getSsn() {
+        return Ssn;
+    }
+
+    public void setSsn(String Ssn) {
+        this.Ssn = Ssn;
+    }
+
+    public String getStr_addr() {
+        return Str_addr;
+    }
+
+    public void setStr_addr(String Str_addr) {
+        this.Str_addr = Str_addr;
+    }
+
+    public String getCity() {
+        return City;
+    }
+
+    public void setCity(String City) {
+        this.City = City;
+    }
+
+    public String getState() {
+        return State;
+    }
+
+    public void setState(String State) {
+        this.State = State;
+    }
+
+    public String getZip() {
+        return Zip;
+    }
+
+    public void setZip(String Zip) {
+        this.Zip = Zip;
+    }
+
+    public String getSave_name() {
+        return Save_name;
+    }
+
+    public void setSave_name(String Save_name) {
+        this.Save_name = Save_name;
+    }
+
+    public String getSave__rnum() {
+        return Save__rnum;
+    }
+
+    public void setSave__rnum(String Save__rnum) {
+        this.Save__rnum = Save__rnum;
+    }
+
+    public String getSave__accnum() {
+        return Save__accnum;
+    }
+
+    public void setSave__accnum(String Save__accnum) {
+        this.Save__accnum = Save__accnum;
+    }
+
+    public String getSave__accbal() {
+        return Save__accbal;
+    }
+
+    public void setSave__accbal(String Save__accbal) {
+        this.Save__accbal = Save__accbal;
+    }
+
+    public String getSave__acctype() {
+        return Save__acctype;
+    }
+
+    public void setSave__acctype(String Save__acctype) {
+        this.Save__acctype = Save__acctype;
+    }
+
+    public String getChk_name() {
+        return Chk_name;
+    }
+
+    public void setChk_name(String Chk_name) {
+        this.Chk_name = Chk_name;
+    }
+
+    public String getChk__rnum() {
+        return Chk__rnum;
+    }
+
+    public void setChk__rnum(String Chk__rnum) {
+        this.Chk__rnum = Chk__rnum;
+    }
+
+    public String getChk__accnum() {
+        return Chk__accnum;
+    }
+
+    public void setChk__accnum(String Chk__accnum) {
+        this.Chk__accnum = Chk__accnum;
+    }
+
+    public String getChk__accbal() {
+        return Chk__accbal;
+    }
+
+    public void setChk__accbal(String Chk__accbal) {
+        this.Chk__accbal = Chk__accbal;
+    }
+
+    public String getChk__acctype() {
+        return Chk__acctype;
+    }
+
+    public void setChk__acctype(String Chk__acctype) {
+        this.Chk__acctype = Chk__acctype;
+    }
+
+    public String getLcnsnum() {
+        return Lcnsnum;
+    }
+
+    public void setLcnsnum(String Lcnsnum) {
+        this.Lcnsnum = Lcnsnum;
+    }
+
+    public String getIss_dt() {
+        return Iss_dt;
+    }
+
+    public void setIss_dt(String Iss_dt) {
+        this.Iss_dt = Iss_dt;
+    }
+
+    public String getExp_dt() {
+        return Exp_dt;
+    }
+
+    public void setExp_dt(String Exp_dt) {
+        this.Exp_dt = Exp_dt;
+    }
+
+    public String getBld_type() {
+        return Bld_type;
+    }
+
+    public void setBld_type(String Bld_type) {
+        this.Bld_type = Bld_type;
+    }
+
+    public String getPicture() {
+        return Picture;
+    }
+
+    public void setPicture(String Picture) {
+        this.Picture = Picture;
+    }
+
+    public String getM_rcrdnum() {
+        return M_rcrdnum;
+    }
+
+    public void setM_rcrdnum(String M_rcrdnum) {
+        this.M_rcrdnum = M_rcrdnum;
+    }
+
+    public String getAlrg_1() {
+        return Alrg_1;
+    }
+
+    public void setAlrg_1(String Alrg_1) {
+        this.Alrg_1 = Alrg_1;
+    }
+
+    public String getAlrg_2() {
+        return Alrg_2;
+    }
+
+    public void setAlrg_2(String Alrg_2) {
+        this.Alrg_2 = Alrg_2;
+    }
+
+    public String getAlrg_3() {
+        return Alrg_3;
+    }
+
+    public void setAlrg_3(String Alrg_3) {
+        this.Alrg_3 = Alrg_3;
+    }
+    
+    
+}
