d945e2a1186e537a7282e9be5819855b3d24591e
ranade-ame <ranade.ame@northeastern.edu>
2021-02-22 19:21:14 -0500

    AED Assignment 2 Part 1 final commit

--- AED_Assignment2_Part1/src/Main.java
+++ AED_Assignment2_Part1/src/Main.java
@@ -21,8 +21,8 @@ public class Main {
         System.out.println("Enter 'Test Cases' to use Test Cases.\nEnter 'User Input' to enter values.\n");
         String option = sc.nextLine(); //Get input from user regarding User Input to Test Cases
         
-        if ("User Input".equals(option)){ //If User Input, get input of Vital Signs variables from console
-            
+        if (("User Input".equals(option)) || ("user input".equals(option))){ //If User Input, get input of Vital Signs variables from console
+
             Patient ptn = new Patient();
             VitalSigns vitalsigns1 = new VitalSigns();
             ptn.getDetails(vitalsigns1);
@@ -38,96 +38,113 @@ public class Main {
             }
             ptn.printhistory(vitalsigns1);
         }
-        
-        if ("Test Cases".equals(option)){ //If Test Cases, select one of the 6 cases and run to get True everytime
-            TestCases ts = new TestCases();
-            VitalSigns vitalsigns = new VitalSigns();
-            Patient ptnTestCase = new Patient();
-            Scanner sc2 = new Scanner(System.in);
-            System.out.println("Select the Test Case:\nNewborn\nInfant\nToddler\nPreschooler\nSchool\nAdolescent"); //Take Input from user to determine which of 6 Test Cases to run
-            String option2 = sc2.nextLine();
-            if("Newborn".equals(option2)){
-                ts.NewbornTestCase(vitalsigns);
-                ptnTestCase.getAgeGroup(vitalsigns);
-                boolean statusTestCase = ptnTestCase.isPatientnormal(vitalsigns);
-                vitalsigns.status = statusTestCase;
-                if (statusTestCase == false){
-                    System.out.println("Patient is abnormal!");
+
+        else if (("Test Cases".equals(option)) || ("test cases".equals(option)) || ("test case".equals(option)) || ("Test Case".equals(option))){ //If Test Cases, select one of the 6 cases and run to get True everytime
+            int x = 0;
+            while(x == 0){
+                TestCases ts = new TestCases();
+                VitalSigns vitalsigns = new VitalSigns();
+                Patient ptnTestCase = new Patient();
+                Scanner sc2 = new Scanner(System.in);
+                System.out.println("Select the Test Case:\nNewborn\nInfant\nToddler\nPreschooler\nSchool\nAdolescent"); //Take Input from user to determine which of 6 Test Cases to run
+                String option2 = sc2.nextLine();
+                if(("Newborn".equals(option2)) || ("newborn".equals(option2))){
+                    ts.NewbornTestCase(vitalsigns);
+                    ptnTestCase.getAgeGroup(vitalsigns);
+                    boolean statusTestCase = ptnTestCase.isPatientnormal(vitalsigns);
+                    vitalsigns.status = statusTestCase;
+                    if (statusTestCase == false){
+                        System.out.println("Patient is abnormal!");
+                    }
+                    else {
+                        System.out.println("Patient is normal!");
+                    }
+                    ptnTestCase.printhistory(vitalsigns);
+                }
+                else if(("Infant".equals(option2)) || ("infant".equals(option2))){
+                    ts.InfantTestCase(vitalsigns);
+                    ptnTestCase.getAgeGroup(vitalsigns);
+                    boolean statusTestCase = ptnTestCase.isPatientnormal(vitalsigns);
+                    vitalsigns.status = statusTestCase;
+                    if (statusTestCase == false){
+                        System.out.println("Patient is abnormal!");
+                    }
+                    else {
+                        System.out.println("Patient is normal!");
+                    }
+                    ptnTestCase.printhistory(vitalsigns);
+                }
+                else if(("Toddler".equals(option2)) || ("toddler".equals(option2))){
+                    ts.TodderTestCase(vitalsigns);
+                    ptnTestCase.getAgeGroup(vitalsigns);
+                    boolean statusTestCase = ptnTestCase.isPatientnormal(vitalsigns);
+                    vitalsigns.status = statusTestCase;
+                    if (statusTestCase == false){
+                        System.out.println("Patient is abnormal!");
+                    }
+                    else {
+                        System.out.println("Patient is normal!");
+                    }
+                    ptnTestCase.printhistory(vitalsigns);
+                }
+                else if(("Preschooler".equals(option2)) || ("preschooler".equals(option2))){
+                    ts.PreschoolerTestCase(vitalsigns);
+                    ptnTestCase.getAgeGroup(vitalsigns);
+                    boolean statusTestCase = ptnTestCase.isPatientnormal(vitalsigns);
+                    vitalsigns.status = statusTestCase;
+                    if (statusTestCase == false){
+                        System.out.println("Patient is abnormal!");
+                    }
+                    else {
+                        System.out.println("Patient is normal!");
+                    }
+                    ptnTestCase.printhistory(vitalsigns);
+                }
+                else if(("School".equals(option2)) || ("school".equals(option2))){
+                    ts.SchoolTestCase(vitalsigns);
+                    ptnTestCase.getAgeGroup(vitalsigns);
+                    boolean statusTestCase = ptnTestCase.isPatientnormal(vitalsigns);
+                    vitalsigns.status = statusTestCase;
+                    if (statusTestCase == false){
+                        System.out.println("Patient is abnormal!");
+                    }
+                    else {
+                        System.out.println("Patient is normal!");
+                    }
+                    ptnTestCase.printhistory(vitalsigns);
+                }
+                else if(("Adolescent".equals(option2)) || ("adolescent".equals(option2))){
+                    ts.AdolescentTestCase(vitalsigns);
+                    ptnTestCase.getAgeGroup(vitalsigns);
+                    boolean statusTestCase = ptnTestCase.isPatientnormal(vitalsigns);
+                    vitalsigns.status = statusTestCase;
+                    if (statusTestCase == false){
+                        System.out.println("Patient is abnormal!");
+                    }
+                    else {
+                        System.out.println("Patient is normal!");
+                    }
+                    ptnTestCase.printhistory(vitalsigns);
                 }
                 else {
-                    System.out.println("Patient is normal!");
+                    System.out.println("Invalid Input ");
                 }
-                ptnTestCase.printhistory(vitalsigns);
-            }
-            if("Infant".equals(option2)){
-                ts.InfantTestCase(vitalsigns);
-                ptnTestCase.getAgeGroup(vitalsigns);
-                boolean statusTestCase = ptnTestCase.isPatientnormal(vitalsigns);
-                vitalsigns.status = statusTestCase;
-                if (statusTestCase == false){
-                    System.out.println("Patient is abnormal!");
+                System.out.println("\nRun another Test Case? Enter 'Yes' or 'No'");
+                String option3 = sc.nextLine();
+                if (("Yes".equals(option3)) || ("yes".equals(option3))){
+                    x = 0;
+                }
+                else if (("No".equals(option3)) || ("no".equals(option3))){
+                    x = 1;
                 }
                 else {
-                    System.out.println("Patient is normal!");
+                    System.out.println("Invalid Input");
                 }
-                ptnTestCase.printhistory(vitalsigns);
             }
-            if("Toddler".equals(option2)){
-                ts.TodderTestCase(vitalsigns);
-                ptnTestCase.getAgeGroup(vitalsigns);
-                boolean statusTestCase = ptnTestCase.isPatientnormal(vitalsigns);
-                vitalsigns.status = statusTestCase;
-                if (statusTestCase == false){
-                    System.out.println("Patient is abnormal!");
-                }
-                else {
-                    System.out.println("Patient is normal!");
-                }
-                ptnTestCase.printhistory(vitalsigns);
-            }
-            if("Preschooler".equals(option2)){
-                ts.PreschoolerTestCase(vitalsigns);
-                ptnTestCase.getAgeGroup(vitalsigns);
-                boolean statusTestCase = ptnTestCase.isPatientnormal(vitalsigns);
-                vitalsigns.status = statusTestCase;
-                if (statusTestCase == false){
-                    System.out.println("Patient is abnormal!");
-                }
-                else {
-                    System.out.println("Patient is normal!");
-                }
-                ptnTestCase.printhistory(vitalsigns);
-            }
-            if("School".equals(option2)){
-                ts.SchoolTestCase(vitalsigns);
-                ptnTestCase.getAgeGroup(vitalsigns);
-                boolean statusTestCase = ptnTestCase.isPatientnormal(vitalsigns);
-                vitalsigns.status = statusTestCase;
-                if (statusTestCase == false){
-                    System.out.println("Patient is abnormal!");
-                }
-                else {
-                    System.out.println("Patient is normal!");
-                }
-                ptnTestCase.printhistory(vitalsigns);
-            }
-            if("Adolescent".equals(option2)){
-                ts.AdolescentTestCase(vitalsigns);
-                ptnTestCase.getAgeGroup(vitalsigns);
-                boolean statusTestCase = ptnTestCase.isPatientnormal(vitalsigns);
-                vitalsigns.status = statusTestCase;
-                if (statusTestCase == false){
-                    System.out.println("Patient is abnormal!");
-                }
-                else {
-                    System.out.println("Patient is normal!");
-                }
-                ptnTestCase.printhistory(vitalsigns);
-            }
-            
         }
-        
-        
+        else {
+            System.out.println("Invalid Input - CASE SENSITIVE");
+        }     
     }
     
 }
